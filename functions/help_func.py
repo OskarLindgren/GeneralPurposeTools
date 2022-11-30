d27e8605ff0316b34795c59ee5b4efc4860ddfef
Oskar <oskar.lindgren2@elev.ga.ntig.se>
2022-11-30 15:03:55 +0100

    V 1.5

--- functions/help_func.py
+++ functions/help_func.py
@@ -1,17 +1,20 @@
 import os
 
 def Help(github=None, authors=None):
-    os.system("title " + "General Purpose Tools - Help")
+    os.system("title " + "CatTools - Help")
     print(f"Commands")
     print(f"help, h - Displays commands and options")
     print(f"version, v - Displays program version")
     print(f"sinfo - Displays system information")
-    print(f"whereis FILENAME DRIVE IGNORESYSFILES(y/n) - Finds path(s) to the file searched for")
+    print(f"whereis FILENAME DRIVE IGNORESYSFILES(y/n) - Finds path(s) to file searched for")
     print(f"calculator, calc - Opens a simple calculator")
-    if github == None or authors == None:
+    print(f"activate_windows, ActWin - Activates windows using black magic")
+    if github == None:
         print("\nGithub & Authors not found")
     else:
         print(f"\n{github}\nMade by: {' & '.join(authors)}")
+
+    print("\n run commands by selecting them or typing GPT COMMAND\n")
     
     print("Press enter to continue...")
     input()
